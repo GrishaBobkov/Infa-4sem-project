a31d783c6253958b35cda61c9d6c2a8b46bfef6e
GrishaBobkov <55443880+GrishaBobkov@users.noreply.github.com>
2020-11-09 20:07:09 +0300

    v0.1
    Now, I can draw the graphics

--- main.cpp
+++ main.cpp
@@ -1,8 +1,34 @@
 #include <iostream>
+#include <SFML/Graphics.hpp>
 
+using namespace sf;
 using namespace std;
 
 int main()
 {
-return 0;
+	// Объект, который, собственно, является главным окном приложения
+	RenderWindow window(VideoMode(800, 600), "My project!");
+
+	// Главный цикл приложения. Выполняется, пока открыто окно
+	while (window.isOpen())
+	{
+		// Обрабатываем очередь событий в цикле
+		Event event;
+		while (window.pollEvent(event))
+		{
+			// Пользователь нажал на «крестик» и хочет закрыть окно?
+			if (event.type == Event::Closed)
+				// Тогда закрываем его
+				window.close();
+		}
+		// Отрисовка окна	
+		window.clear(Color(250, 220, 100, 0));
+		CircleShape ob1(40);
+		ob1.setPosition({ 20, 20 });
+		ob1.setFillColor(Color(0xFF, 0x0, 0x0));
+		window.draw(ob1);
+		window.display();
+	}
+
+	return 0;
 }
