cfb6481a5c1f8c18f592a0715c4f09d5ba74f68a
GrishaBobkov <55443880+GrishaBobkov@users.noreply.github.com>
2020-11-28 22:47:33 +0300

    v0.6
    Add normal form of force, add opp. to change energy

--- main.cpp
+++ main.cpp
@@ -1,26 +1,22 @@
 #include <iostream>
 #include <SFML/Graphics.hpp>
 #include <vector>
-#include <set>
+#include <map>
 #include <math.h>
 
 using namespace sf;
 using namespace std;
 
-double dt = 0.001;//Единица времени - 1мкс
-double dx = 0.01;//Еденица длины - 1мм
-double Lx = 1000;
-double Ly = 1000;
-int smp = 100;
+double dt = 0.01;
 
 pair<double, double> force(double x, double y)
-{	
+{
 	pair<double, double> f;
-	double l, f0;
-	l = sqrt(x * x + y + y);
-	f0 = 0,00015/dx*(pow(2, (dx-l) / (0.01 * dx))-1);
-	if (l < 1.01 * dx)
+	if (abs(x) + abs(y) < 100)
 	{
+		double l, f0;
+		l = sqrt(x * x + y * y);
+		f0 = 100/pow(l, 4)-60000/pow(l, 8);
 		if (x != 0)f.first = f0 * x / l;
 		else f.first = 0;
 		if (y != 0)f.second = f0 * y / l;
@@ -41,9 +37,8 @@ public:
 	double y;
 	double vx;
 	double vy;
-	int n;
 	pair <double, double> f;
-	CircleShape id; 
+	CircleShape id;
 	Ball()
 	{
 		x = 0;
@@ -65,30 +60,21 @@ public:
 		window->draw(id);
 	}
 
-	void choose_place(vector<vector<vector<Ball*>>>& the_nearest_balls)
+	void calculate_force(const vector<Ball*> &all_balls)
 	{
-		int xlocal, ylocal;
-		xlocal = x * smp / Lx;
-		ylocal = y * smp / Ly;
-		xlocal = xlocal % smp;
-		ylocal = ylocal % smp;
-		//the_nearest_balls[xlocal][ylocal].push_back(this);
-	}
-
-	void calculate_force(const vector<vector<vector<Ball*>>>& the_nearest_balls)
-	{
-		int xlocal, ylocal;
-		xlocal = x * smp / Lx;
-		ylocal = y * smp / Ly;
 		pair<double, double> flocal;
 		f.first = 0; f.second = 0;
-		/*for(int i=-1; i<2; i++)for (int j = -1; j < 2; j++)
-		for (auto b : the_nearest_balls[(xlocal+i)%smp][(ylocal+j)%smp])
+		for (auto b : all_balls)
 		{
 			flocal = force(b->x - x, b->y - y);
 			f.first += flocal.first;
 			f.second += flocal.second;
-		}*/
+		}
+		if (x < 200)f.first += dt * (200 - x) / 10;
+		if (x > 800)f.first -= dt * (x - 800) / 10;
+		if (y < 200)f.second += dt * (200 - y) / 10;
+		if (y > 800)f.second -= dt * (y - 800) / 10;
+
 	}
 
 	void live()
@@ -98,38 +84,44 @@ public:
 		x += vx * dt;
 		y += vy * dt;
 	}
+
+	void lose_energy()
+	{
+		vx = vx * 0.9;
+		vy = vy * 0.9;
+	}
+
+	void rise_energy()
+	{
+		vx = vx * 1.1;
+		vy = vy * 1.1;
+	}
 };
 
-void empty_the_nearest_balls(const vector<vector<vector<Ball*>>>& the_nearest_balls)
+void cold(const vector<Ball*> all_balls)
 {
-	for (auto i : the_nearest_balls)for (auto j : i)j.clear();
+	for (auto b : all_balls)b->lose_energy();
+}
+
+void heat(const vector<Ball*> all_balls)
+{
+	for (auto b : all_balls)b->rise_energy();
 }
 
 int main()
 {
 	vector<Ball*> all_balls;
-	vector<vector<vector<Ball*>>> the_nearest_balls;
-	the_nearest_balls.resize(smp);
-	for (auto i : the_nearest_balls)i.resize(smp);
-	const int Number_balls = 10100, fps=1000;
-	for (int i = 0; i < Number_balls; i++) 
-	{ 
-		Ball* ball = new Ball; 
-		all_balls.push_back(ball); 
+	const int Number_balls = 100, fps = 10;
+	int L=sqrt(Number_balls);
+	for (int i = 0; i < Number_balls; i++)
+	{
+		Ball* ball = new Ball;
+		all_balls.push_back(ball);
 	}
-	for (int i = 0; i < Number_balls; i++) 
-	{ 
-		if (i < 10000)
-		{
-			all_balls[i]->x = (i - i % 100) / 20 + 200;
-			all_balls[i]->y = i % 100 * 5 + 300;
-		}
-		else
-		{
-			all_balls[i]->x = (i-10000 - i % 10) / 2 + 900;
-			all_balls[i]->y = i % 10 * 5 + 500;
-			all_balls[i]->vx = -10;
-		}
+	for (int i = 0; i < Number_balls; i++)
+	{
+		all_balls[i]->x = (i - i % L) * 10 / L + 400;
+		all_balls[i]->y = i % L * 10 + 400;
 	}
 
 	RenderWindow window(VideoMode(1400, 1000), "My project!");
@@ -139,20 +131,17 @@ int main()
 		Event event;
 		while (window.pollEvent(event))
 		{
+			if (event.type == Event::KeyPressed && event.key.code == sf::Keyboard::Down)cold(all_balls);
+			if (event.type == Event::KeyPressed && event.key.code == sf::Keyboard::Up)heat(all_balls);
 			if (event.type == Event::Closed)
 				window.close();
 		}
 		window.clear(Color(150, 150, 255, 0));
 		for (int k = 0; k < fps; k++)
 		{
-			empty_the_nearest_balls(the_nearest_balls);
 			for (int i = 0; i < Number_balls; i++)
 			{
-				all_balls[i]->choose_place(the_nearest_balls);
-			}
-			for (int i = 0; i < Number_balls; i++)
-			{
-				all_balls[i]->calculate_force(the_nearest_balls);
+				all_balls[i]->calculate_force(all_balls);
 			}
 			for (int i = 0; i < Number_balls; i++)
 			{
@@ -160,7 +149,7 @@ int main()
 			}
 		}
 		for (int i = 0; i < Number_balls; i++) { all_balls[i]->show(&window); }
-		
+		//sf::sleep(sf::milliseconds(2000));
 		window.display();
 	}
 	for (auto b : all_balls)delete b;
