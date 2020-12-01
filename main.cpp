fe9f289e54f87c248b5a9b38dd96ebcabf1977b9
GrishaBobkov <55443880+GrishaBobkov@users.noreply.github.com>
2020-12-01 16:33:45 +0300

    v0.7
    final(almost) result for calc. dynamic particles
    started presentation for data of system

--- main.cpp
+++ main.cpp
@@ -3,20 +3,38 @@
 #include <vector>
 #include <map>
 #include <math.h>
+#include <random>
+#include <chrono>
+#include <fstream>
 
 using namespace sf;
 using namespace std;
 
+double get_time()
+{
+	return std::chrono::duration_cast<std::chrono::microseconds>(std::chrono::steady_clock::now().time_since_epoch()).count() / 1e6;
+}
+
+int rand_uns(int min, int max)
+{
+	unsigned seed = std::chrono::steady_clock::now().time_since_epoch().count();
+	static std::default_random_engine e(seed);
+	std::uniform_int_distribution<int> d(min, max);
+	return d(e);
+}
+
 double dt = 0.01;
+bool heating_turn = false;
+double Power_heating = 0.001;
 
 pair<double, double> force(double x, double y)
 {
 	pair<double, double> f;
-	if (abs(x) + abs(y) < 100)
+	if (abs(x) + abs(y) >5)
 	{
 		double l, f0;
 		l = sqrt(x * x + y * y);
-		f0 = 100/pow(l, 4)-60000/pow(l, 8);
+		f0 = 20000/pow(l, 4)-1000000000/pow(l, 8);
 		if (x != 0)f.first = f0 * x / l;
 		else f.first = 0;
 		if (y != 0)f.second = f0 * y / l;
@@ -43,8 +61,10 @@ public:
 	{
 		x = 0;
 		y = 0;
-		vx = 0;
-		vy = 0;
+		vx = rand_uns(-1000, 1000);
+		vy = rand_uns(-1000, 1000);
+		vx = vx / 1000;
+		vy = vy / 1000;
 		f.first = 0;
 		f.second = 0;
 		id.setRadius(4);
@@ -53,6 +73,10 @@ public:
 
 	void show(sf::RenderWindow* window)
 	{
+		int color;
+		color = (int)(sqrt(vx * vx + vy * vy)*50);
+		if (color > 255)color = 255;
+		id.setFillColor(Color(color, 0, 255- color));
 		sf::Vector2f k;
 		k.x = (int)x;
 		k.y = (int)y;
@@ -63,22 +87,26 @@ public:
 	void calculate_force(const vector<Ball*> &all_balls)
 	{
 		pair<double, double> flocal;
-		f.first = 0; f.second = 0;
+		f.first = 0; f.second = 0.001;
 		for (auto b : all_balls)
 		{
 			flocal = force(b->x - x, b->y - y);
 			f.first += flocal.first;
 			f.second += flocal.second;
 		}
-		if (x < 200)f.first += dt * (200 - x) / 10;
-		if (x > 800)f.first -= dt * (x - 800) / 10;
-		if (y < 200)f.second += dt * (200 - y) / 10;
-		if (y > 800)f.second -= dt * (y - 800) / 10;
-
+		if (x < 100)f.first += (100 - x)/10;
+		if (x > 900)f.first -= (x - 900) / 10;
+		if (y < 100)f.second += (100 - y) / 10;
+		if (y > 900)f.second -= (y - 900) / 10;
 	}
 
 	void live()
 	{
+		if (heating_turn)
+		{
+			vx += vx / (vx * vx + vy * vy) * Power_heating * dt;
+			vy += vy / (vx * vx + vy * vy) * Power_heating * dt;
+		}
 		vx += f.first * dt;
 		vy += f.second * dt;
 		x += vx * dt;
@@ -93,8 +121,24 @@ public:
 
 	void rise_energy()
 	{
-		vx = vx * 1.1;
-		vy = vy * 1.1;
+		if (abs(vx) < 3)vx = vx * 1.1;
+		if (abs(vy) < 3)vy = vy * 1.1;
+	}
+};
+
+class graph
+{
+	vector<pair<double, double>> data;
+	int x;
+	int y;
+public:
+	graph(vector<pair<double, double>> &d)
+	{
+
+	}
+	~graph()
+	{
+
 	}
 };
 
@@ -111,7 +155,7 @@ void heat(const vector<Ball*> all_balls)
 int main()
 {
 	vector<Ball*> all_balls;
-	const int Number_balls = 100, fps = 10;
+	const int Number_balls = 25, fps = 10;
 	int L=sqrt(Number_balls);
 	for (int i = 0; i < Number_balls; i++)
 	{
@@ -120,12 +164,20 @@ int main()
 	}
 	for (int i = 0; i < Number_balls; i++)
 	{
-		all_balls[i]->x = (i - i % L) * 10 / L + 400;
-		all_balls[i]->y = i % L * 10 + 400;
+		all_balls[i]->x = (i - i % L) * 15 / L + 400;
+		all_balls[i]->y = i % L * 15 + 400;
 	}
 
 	RenderWindow window(VideoMode(1400, 1000), "My project!");
-
+	sf::RectangleShape fon;
+	fon.setFillColor(Color(150, 150, 255));
+	sf::Vector2f k;
+	k.x = 800;
+	k.y = 800;
+	fon.setSize(k);
+	k.x = 100;
+	k.y = 100;
+	fon.setPosition(k);
 	while (window.isOpen())
 	{
 		Event event;
@@ -133,10 +185,13 @@ int main()
 		{
 			if (event.type == Event::KeyPressed && event.key.code == sf::Keyboard::Down)cold(all_balls);
 			if (event.type == Event::KeyPressed && event.key.code == sf::Keyboard::Up)heat(all_balls);
+			if (event.type == Event::KeyPressed && event.key.code == sf::Keyboard::H)heating_turn = true;
+			if (event.type == Event::KeyPressed && event.key.code == sf::Keyboard::C)heating_turn = false;
 			if (event.type == Event::Closed)
 				window.close();
 		}
-		window.clear(Color(150, 150, 255, 0));
+		window.clear(Color(100, 100, 200, 0));
+		window.draw(fon);
 		for (int k = 0; k < fps; k++)
 		{
 			for (int i = 0; i < Number_balls; i++)
