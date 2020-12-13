12e312bf349057fde9305b54c3bae04f268e34dc
GrishaBobkov <55443880+GrishaBobkov@users.noreply.github.com>
2020-12-13 17:23:44 +0300

    v0.9
    Add text, add Bolzm.-distr.

--- main.cpp
+++ main.cpp
@@ -24,6 +24,8 @@ int rand_uns(int min, int max)
 	return d(e);
 }
 
+sf::Font font;
+
 double dt = 0.01;
 int size_of_graph_x = 350;
 int size_of_graph_y = 150;
@@ -33,7 +35,7 @@ int diagram_size=100;
 int heating_turn = 0;
 bool centre = false;
 double Power_heating = 0.01;
-double gravity = 0.01;
+double gravity = 0.00;
 
 pair<double, double> force(double x, double y)
 {
@@ -163,7 +165,7 @@ public:
 		id.clear();
 		sf::Vertex* new_vertex = new sf::Vertex;
 		new_vertex->position = sf::Vector2f(x + started_data.first, y + started_data.second);
-		new_vertex->color = sf::Color(0, 255, 0);
+		new_vertex->color = sf::Color(0, 0, 255);
 		all_vertex.push_back(new_vertex);
 		id.append(*new_vertex);
 
@@ -172,15 +174,18 @@ public:
 		v1.position = sf::Vector2f(x + size_of_graph_x, y);
 		v2.position = sf::Vector2f(x , y);
 		v3.position = sf::Vector2f(x, y- size_of_graph_y);
-		v1.color = sf::Color(255, 0, 0);
-		v2.color = sf::Color(255, 0, 0);
-		v3.color = sf::Color(255, 0, 0);
+		v1.color = sf::Color(0, 0, 0);
+		v2.color = sf::Color(0, 0, 0);
+		v3.color = sf::Color(0, 0, 0);
 		axes.append(v1);
 		axes.append(v2);
 		axes.append(v3);
-		textid.setPosition(sf::Vector2f(500, 500));/*x + size_of_graph_x / 2, y + size_of_graph_y + 20*/
+
+		textid.setPosition(sf::Vector2f(x, y));
 		textid.setString(name);
-		//textid.setColor(sf::Color(255, 0, 0));
+		textid.setFont(font);
+		textid.setCharacterSize(25);
+		textid.setFillColor(sf::Color(0, 0, 0));
 	}
 
 	~graph()
@@ -192,7 +197,7 @@ public:
 	{
 		window->draw(id);
 		window->draw(axes);
-		//window->draw(textid);
+		window->draw(textid);
 	}
 
 	void push(pair<double, double> new_data)
@@ -200,7 +205,7 @@ public:
 		data.push_back(new_data);
 		sf::Vertex* new_vertex = new sf::Vertex;
 		new_vertex->position = sf::Vector2f(x, y);
-		new_vertex->color = sf::Color(0, 255, 0);
+		new_vertex->color = sf::Color(0, 0, 255);
 		all_vertex.push_back(new_vertex);
 		id.append(*new_vertex);
 
@@ -228,6 +233,7 @@ class diagram
 	sf::VertexArray id;
 	sf::VertexArray axes;
 	sf::Text textid;
+
 	int x;
 	int y;
 	int size;
@@ -247,7 +253,7 @@ public:
 		{
 			sf::Vertex* new_vertex = new sf::Vertex;
 			new_vertex->position = sf::Vector2f(x, y);
-			new_vertex->color = sf::Color(0, 255, 0);
+			new_vertex->color = sf::Color(0, 0, 255);
 			all_vertex.push_back(new_vertex);
 			id.append(*new_vertex);
 		}
@@ -257,15 +263,18 @@ public:
 		v1.position = sf::Vector2f(x + size_of_graph_x, y);
 		v2.position = sf::Vector2f(x, y);
 		v3.position = sf::Vector2f(x, y - size_of_graph_y);
-		v1.color = sf::Color(255, 0, 0);
-		v2.color = sf::Color(255, 0, 0);
-		v3.color = sf::Color(255, 0, 0);
+		v1.color = sf::Color(0, 0, 0);
+		v2.color = sf::Color(0, 0, 0);
+		v3.color = sf::Color(0, 0, 0);
 		axes.append(v1);
 		axes.append(v2);
 		axes.append(v3);
-		textid.setPosition(sf::Vector2f(500, 500));/*x + size_of_graph_x / 2, y + size_of_graph_y + 20*/
+
+		textid.setPosition(sf::Vector2f(x, y));
 		textid.setString(name);
-		//textid.setColor(sf::Color(255, 0, 0));
+		textid.setFont(font);
+		textid.setCharacterSize(25);
+		textid.setFillColor(sf::Color(0, 0, 0));
 	}
 
 	~diagram()
@@ -277,7 +286,7 @@ public:
 	{
 		window->draw(id);
 		window->draw(axes);
-		//window->draw(textid);
+		window->draw(textid);
 	}
 
 	void push(vector<pair<double, double>> new_data)
@@ -342,6 +351,25 @@ vector<pair<double, double>> count_bolzman(const list<Ball*>& all_balls)
 	return local_data;
 }
 
+vector<pair<double, double>> count_makswell(const list<Ball*>& all_balls)
+{
+	double v;
+	vector<pair<double, double>> local_data;
+	pair<double, double> local_pair;
+	for (double i = 0; i < diagram_size; i++)
+	{
+		local_pair.first = 10 * i / diagram_size;
+		local_pair.second = 0;
+		for (auto b : all_balls)
+		{
+			v = sqrt(b->vy * b->vy + b->vx + b->vx);
+			if (v >= 10 * i / diagram_size && v < 10 * (i + 1) / diagram_size)local_pair.second += 1;
+		}
+		local_data.push_back(local_pair);
+	}
+	return local_data;
+}
+
 void centre_active(const list<Ball*>& all_balls)
 {
 	double Vx=0, Vy=0, N=0;
@@ -364,7 +392,7 @@ int main()
 	list<Ball*> all_balls;
 	list<graph*> all_graphs;
 	list<diagram*> all_diagrams;
-	const int Number_balls = 300, fps = 10;
+	const int Number_balls = 110, fps = 10;
 	int L=sqrt(Number_balls), j;
 	double current_time = 0;
 	int shag = 0;
@@ -376,24 +404,44 @@ int main()
 	j = 0;
 	for (auto i : all_balls)
 	{
-		i->x = (j - j % L) * 15 / L + 400;
-		i->y = j % L * 15 + 660;
-		j++;
+		if (j < 100)
+		{
+			i->x = j % L * 15 + 400;
+			i->y = (j - j % L) * 15 / L + 700;
+			j++;
+		}
+		else
+		{
+			i->x = j % 3 * 15 + 460;
+			i->y = (j - j % 3) * 15 / 3 - 200;
+			j++;
+		}
+
 	}
 	pair<double, double> local_data;
 	local_data.first = 0; local_data.second = 0;
-	graph gr1(local_data, 0, "Energy");
+	graph gr1(local_data, 0, "Temperature");
 	all_graphs.push_back(&gr1);
-	graph gr2(local_data, 1, "Temperature");
+	graph gr2(local_data, 1, "Un-ready");
 	all_graphs.push_back(&gr2);
 	diagram d1(diagram_size, 3, "Bolzman");
 	all_diagrams.push_back(&d1);
+	diagram d2(diagram_size, 2, "Makswell");
+	all_diagrams.push_back(&d2);
 
 	RenderWindow window(VideoMode(1400, 1000), "My project!");
 	sf::RectangleShape fon;
 	fon.setFillColor(Color(150, 150, 255));
 	fon.setSize(sf::Vector2f(800, 800));
 	fon.setPosition(sf::Vector2f(100, 100));
+	font.loadFromFile("CyrilicOld.ttf");
+	sf::Text tx1;
+	tx1.setPosition(sf::Vector2f(0,0));
+	tx1.setString("Управление: клавиши-стрелки вверх и вниз - нагреть и охладить систему.\n H и C - соответственно включить адиабатический нагрев и охлаждение\n T - отключить гравитацию, G - включить/сменить направление гравитации.");
+	tx1.setFont(font);
+	tx1.setCharacterSize(30);
+	tx1.setFillColor(sf::Color(0, 0, 0));
+
 	while (window.isOpen())
 	{
 		Event event;
@@ -404,11 +452,14 @@ int main()
 			if (event.type == Event::KeyPressed && event.key.code == sf::Keyboard::H)if (heating_turn < 1)heating_turn +=1;
 			if (event.type == Event::KeyPressed && event.key.code == sf::Keyboard::C)if (heating_turn > -1)heating_turn -= 1;
 			if (event.type == Event::KeyPressed && event.key.code == sf::Keyboard::N) { if (centre == false)centre = true; else centre = false; }
+			if (event.type == Event::KeyPressed && event.key.code == sf::Keyboard::G)if (gravity == -0.01)gravity = 0.01;else gravity = -0.01;
+			if (event.type == Event::KeyPressed && event.key.code == sf::Keyboard::T)gravity=0;
 			if (event.type == Event::Closed)
 				window.close();
 		}
-		window.clear(Color(100, 100, 200, 0));
+		window.clear(Color(200, 200, 200, 0));
 		window.draw(fon);
+		window.draw(tx1);
 		for (int k = 0; k < fps; k++)
 		{
 			current_time += dt;
@@ -428,6 +479,7 @@ int main()
 			local_data = count_energy(all_balls, current_time);
 			gr1.push(local_data);
 			d1.push(count_bolzman(all_balls));
+			d2.push(count_makswell(all_balls));
 		}
 		for (auto i : all_graphs)i->show(&window);
 		for (auto i : all_diagrams)i->show(&window);
@@ -438,3 +490,4 @@ int main()
 	for (auto b : all_balls)delete b;
 	return 0;
 }
+
